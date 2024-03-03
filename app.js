8c08a4a1d633f9d0d66080709e3c1c651a6e5f7d
UmarFarooq <153651315+UmerFaooq46@users.noreply.github.com>
2024-03-03 21:21:20 +0500

    submit
    first comment

---
+++ app.js
@@ -0,0 +1,7 @@
+ var fullname = window.prompt("what is your name? ");
+ var age = window.prompt(" how old are you?");
+ var education = window.prompt("you education?");
+
+document.write("Hey "  + fullname + ", How are you today. Do you like being " + age +"years old?");
+
+
